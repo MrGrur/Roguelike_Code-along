2da4b2e6fc6c044c9be9d6bdd9251d105140fdae
MrGrur <gabelife@hotmail.com>
2020-06-23 00:52:09 +1000

    Added numpad diagonal movement support

--- input_handlers.py
+++ input_handlers.py
@@ -12,16 +12,24 @@ class EventHandler(tcod.event.EventDispatch[Action]):
 
         key = event.sym
 
-        if key == tcod.event.K_UP or key == tcod.event.K_KP_8:
-            action = MovementAction(dx=0, dy=-1)
-        elif key == tcod.event.K_DOWN or key == tcod.event.K_KP_2:
+        if key == tcod.event.K_KP_1:
+            action = MovementAction(dx=-1, dy=-1)
+        elif key == tcod.event.K_KP_2 or key == tcod.event.K_DOWN:
             action = MovementAction(dx=0, dy=1)
-        elif key == tcod.event.K_LEFT or key == tcod.event.K_KP_4:
+        elif key == tcod.event.K_KP_3:
+            action = MovementAction(dx=1, dy=-1)
+        elif key == tcod.event.K_KP_4 or key == tcod.event.K_LEFT:
             action = MovementAction(dx=-1, dy=0)
-        elif key == tcod.event.K_RIGHT or key == tcod.event.K_KP_6:
-            action = MovementAction(dx=1, dy=0)
-        elif key == tcod.event.K_PERIOD or key == tcod.event.K_KP_5:
+        elif key == tcod.event.K_KP_5 or key == tcod.event.K_PERIOD:
             action = MovementAction(dx=0, dy=0)
+        elif key == tcod.event.K_KP_6 or key == tcod.event.K_RIGHT:
+            action = MovementAction(dx=1, dy=0)
+        elif key == tcod.event.K_KP_7:
+            action = MovementAction(dx=-1, dy=1)
+        elif key == tcod.event.K_KP_8 or key == tcod.event.K_UP:
+            action = MovementAction(dx=0, dy=-1)
+        elif key == tcod.event.K_KP_9:
+            action = MovementAction(dx=1, dy=1)
 
         elif key == tcod.event.K_ESCAPE:
             action = EscapeAction()
